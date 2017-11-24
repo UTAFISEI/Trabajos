edba74a576619230e588778a169f4963a9f65b6b
remileo <33031388+remileo@users.noreply.github.com>
2017-11-24 15:09:17 -0500

    Update EjemplosGit.java

--- src/ejemplosgit/EjemplosGit.java
+++ src/ejemplosgit/EjemplosGit.java
@@ -19,7 +19,7 @@ public class EjemplosGit {
         
         System.out.println("HOLAAAAAAAAAAAAAAAAAAA");
         
-        stem.out.println("HOLAAAAAAAAAAAAAAAAAAA");
+        Sstem.out.println("HOLAAAAAAAAAAAAAAAAAAA");
     }
     
 }
